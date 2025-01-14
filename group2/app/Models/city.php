71d14d64cd2a7292aef12d191d1beb85f15109d6
Achi Beridze <achiberidze@178-134-71-106.dsl.utg.ge>
2025-01-14 16:35:04 +0400

    bug fix

--- group2/app/Models/city.php
+++ group2/app/Models/city.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class city extends Model
+class City extends Model
 {
-    protected $table = 'city';
-  
-    protected $fillable = ['name', 'countrty_id', 'population'];
+    protected $table = 'cities';
+    protected $fillable = ['name'];
 
     public function country(){
-        return $this->belongsTo(country::class, 'country_id', 'id');
-    }
+        return $this->belongsTo(Country::class , 'country_id', 'id');
+  }
+
 }
--- group2/database/migrations/2025_01_13_180359_create_cities_table.php
+++ group2/database/migrations/2025_01_13_180359_create_cities_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('country_id');
             $table->timestamps();
         });
 
